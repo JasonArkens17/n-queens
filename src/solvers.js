328b306eac6a01db32c710bfe1c56d2d6df69d2f
Jason Davenport <JasonArkens17@users.noreply.github.com>
2017-02-16 17:55:38 -0800

    Update solvers.js

--- src/solvers.js
+++ src/solvers.js
@@ -5,14 +5,7 @@
   |___/\___/|_| \_/ \___|_|  |___/
 */
 
-// hint: you'll need to do a full-search of all possible arrangements of pieces!
-// (There are also optimizations that will allow you to skip a lot of the dead search space)
-// take a look at solversSpec.js to see what the tests are expecting
-
-
-// return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
-
-
+// return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other.
 window.findSolution = function(row, n, board, validator, callback) {
   // if all rows exhausted, this is a valid solution.
   if (row === n) {
